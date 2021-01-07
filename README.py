fd85aad9a2cec050c41767004b291904ce8e1b5c
msp2021 <77120216+msp2021@users.noreply.github.com>
2021-01-07 13:33:46 -0500

    Update README.py

--- README.py
+++ README.py
@@ -1,13 +1,38 @@
 # Metro-Poly
-#first level
-#introduction
+
+# first level/introduction: The player will start game with
+
+# choosing between working or studying
+work_or_study = input("Choose to work or study")
+
+if (work_or_study == "work"):
+  full_time = False
+  num_hours = input("How many hours do you want to work?")
+  # decides whether desired number of hours is part-time or full-time
+  if (num_hours >= 40):
+    full_time = True
+  if (full_time == False):
+    a = input("Do you want to study while working?(Y/N)")
+    if (a == "Y"):
+      work_with_study = True
+    else:
+      work_with_study = False
+    
+    
+
+
+# choosing a part or a full time job
+part_or_full = input("Choose part-time or full-time job")
+# start choosing where to save the money in a bank (have at least two banks)
+bank = input("Choose Bank A or Bank B")
 
 
 
 
 
-
-
-
+# Decide how many hours to spend for college and how many hours of study
+# Decide how many hours of work
+# Decide how many hours of free time
+# Decide how many hours of rest
 
 print()
