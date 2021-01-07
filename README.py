36658b331e24fd06869d8b13408b922862ffcc7e
msp2021 <77120216+msp2021@users.noreply.github.com>
2021-01-07 14:14:01 -0500

    Update README.py

--- README.py
+++ README.py
@@ -5,8 +5,11 @@
 # choosing between working or studying
 work_or_study = input("Choose to work or study: ")
 
+full_time = False
+part_time_with_study = False
+study = False
+
 if (work_or_study == "work"):
-  full_time = False
   num_hours = int(input("How many hours do you want to work? "))
   # decides whether desired number of hours is part-time or full-time
   if (num_hours >= 40):
@@ -14,20 +17,15 @@ if (work_or_study == "work"):
   if (full_time == False):
     a = input("Do you want to study while working?(Y/N) ")
     if (a == "Y"):
-      work_with_study = True
-    else:
-      work_with_study = False
-    
-    
+      part_time_with_study = True
+      
+if (work_or_study == "study"):
+  study = input("Do you want to do a part-time job to study?")
+  
 
 
-# choosing a part or a full time job
-part_or_full = input("Choose part-time or full-time job")
 # start choosing where to save the money in a bank (have at least two banks)
 bank = input("Choose Bank A or Bank B")
-
-
-
 
 
 # Decide how many hours to spend for college and how many hours of study
@@ -35,4 +33,4 @@ bank = input("Choose Bank A or Bank B")
 # Decide how many hours of free time
 # Decide how many hours of rest
 
-print()
+"_____________________________________________________________________________________________________________________"
