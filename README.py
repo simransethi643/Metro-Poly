7ed4b2113ec7807bfd70f105b1caa1a79f94bfcf
simransethi643 <71235263+simransethi643@users.noreply.github.com>
2021-01-08 10:24:22 -0500

    Update README.py

--- README.py
+++ README.py
@@ -34,5 +34,26 @@ bank = input("Choose Bank A or Bank B")
 # Decide how many hours of rest
 
 "_____________________________________________________________________________________________________________________"
-if (work_or_study == "study"):
-  study = input("Do you want to do a part-time job to study while working?")
+# choosing between working or studying
+work_or_study = input("Choose to work or study: ")
+
+full_time = False
+part_time_with_study = False
+study = False
+
+if (work_or_ work_and_study_or_study == "work"):
+  num_hours = int(input("How many hours do you want to work? "))
+  # decides whether desired number of hours is part-time or full-time
+  if (num_hours >= 40):
+    full_time = True
+  if (full_time == False):
+      
+if (work_and_study == "study"):
+  study = input("Do you want to do a part-time job with study?")
+      a = input("Do you want to do a part-time job with study?(Y/N) ")
+    if (a == "Y"):
+      part_time_with_study = True
+   if (work_and_study == False):
+    
+# start choosing where to save the money in a bank (have at least two banks)
+bank = input("Choose Bank A or Bank B")
